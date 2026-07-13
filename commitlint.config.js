c976da2b3511ac4c1efe4f6d6dc0a5b48997a409
Mateo Scarafia <mateoscarafia.acidlabs@latam.com>
2026-07-13 09:38:41 -0300

    feat: improve ci

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // New feature
+        "fix", // Bug fix
+        "docs", // Documentation
+        "style", // Code style (formatting, etc)
+        "refactor", // Code refactoring
+        "perf", // Performance improvements
+        "test", // Testing
+        "chore", // Build process or auxiliary tools
+        "revert", // Revert a commit
+        "ci", // CI/CD changes
+      ],
+    ],
+    "subject-case": [2, "always", "lower-case"],
+    "subject-max-length": [2, "always", 100],
+  },
+};
--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Executed" >  deploy.txt
-
-echo "✅ deploy.txt actualizado con 'Executed'"
